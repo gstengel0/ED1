7e168a36ed2b6a993b702c5e6609629859e9eaca
gstengel0 <gstengel@greenwichacademy.org>
2022-01-13 07:59:49 -0500

    update

--- MachDraw/export_svg/sketch.js
+++ MachDraw/export_svg/sketch.js
@@ -1,28 +1,30 @@
-var pW = 11
-var pH = 8.5
-var x = 0;
-var y = 0;
+/* export SVG
+DDF 2019
+need to have p5.svg.js in project and in index.html
+see -https://github.com/zenozeng/p5.js-svg
+this will save an SVG file in your download folder
+*/
 
-function setup () {
-  createCanvas(pW * 72, pH * 72, SVG);
-  strokeWeight(1);
-  stroke(255, 0, 0);
-  noFill();
-  background(230);
-  rectMode(CENTER);
-  translate (width/2, height/2);
-
-  do{
-    rect (0, 0, 1 + x, 1 + y);
-    x = x + 0
-    y = y + 10
-  }
-  while (x < 600);
+function setup() {
+  createCanvas(600, 600, SVG); // Create SVG Canvas
+  strokeWeight(1); // do 0.1 for laser
+  stroke(255, 0, 0); // red is good for laser
+  noFill(); // better not to have a fill for laser
 
 }
 
+
 function draw() {
-  //save("mySVG.svg");
-  //print("saved svg");
-  noLoop();
+
+translate (width/2 + 20, height/2 + 20);
+for (var y = -300; y < 300; y = y + 40){
+for (var x = -300; x < 300; x = x + 40){
+line (0, 0, x, y);
+}
+}
+
+
+  save("starburstV2.svg"); // give file name
+  print("saved svg");
+  noLoop(); // we just want to export once
 }
